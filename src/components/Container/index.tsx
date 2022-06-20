997a3287e6c7ec221cbf680ea773aa7cc3459363
Caio Barilli <caiobarilli@gmail.com>
2022-06-19 22:42:55 -0300

    Add Larger container for huge screens

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,8 +1,19 @@
 import styled, { css } from 'styled-components'
 
-export const Container = styled.main`
-  ${({ theme }) => css`
-    max-width: ${theme.grid.container};
+type ContainerProps = {
+  larger?: boolean
+}
+
+export const Container = styled.main<ContainerProps>`
+  ${({ theme, larger }) => css`
+    ${larger
+      ? css`
+          max-width: ${theme.grid.containerLarge};
+        `
+      : css`
+          max-width: ${theme.grid.container};
+        `}
+
     margin-left: auto;
     margin-right: auto;
     padding-left: calc(${theme.grid.gutter} / 2);
