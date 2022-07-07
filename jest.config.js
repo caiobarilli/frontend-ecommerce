459753a6de67a7ecac079283be1ffece10eedf53
Caio Barilli <caiobarilli@gmail.com>
2022-07-07 05:37:56 -0300

    Test ajusts

--- jest.config.js
+++ jest.config.js
@@ -5,13 +5,17 @@ module.exports = {
   collectCoverageFrom: [
     'src/**/*.ts(x)?',
     '!src/types/**/*.d.ts',
-    '!src/**/stories.tsx',
     '!src/pages/**/*.tsx',
     '!src/styles/**/*.ts',
+    '!src/**/stories.tsx',
     '!src/**/mock.ts'
   ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
+  moduleNameMapper: {
+    '^styled-components':
+      '<rootDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js'
+  },
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
   }
