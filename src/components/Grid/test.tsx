1fe8d00eca1c0e25e0ff9a0d6b3d6cfa3de833bc
Caio Barilli <caiobarilli@gmail.com>
2022-07-12 21:49:37 -0300

    Add grid component

---
+++ src/components/Grid/test.tsx
@@ -0,0 +1,9 @@
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import { Grid } from '.'
+
+describe('<Grid />', () => {
+  it('should render correctly', () => {
+    renderWithTheme(<Grid>Children</Grid>)
+  })
+})
