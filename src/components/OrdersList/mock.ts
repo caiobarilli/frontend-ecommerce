4eaf8a98486cf9f59ae6f7dae84b72eb7eaac5d5
Caio Barilli <caiobarilli@gmail.com>
2022-07-14 20:09:34 -0300

    Add Order List component

---
+++ src/components/OrdersList/mock.ts
@@ -0,0 +1,26 @@
+export default [
+  {
+    img: 'https://api.lorem.space/image/game?w=151&h=70',
+    title: 'Red Dead Redemption 2',
+    price: 'R$ 215,00',
+    downloadLink: 'https://api.lorem.space/image/game?w=151&h=72',
+    paymentInfo: {
+      flag: 'mastercard',
+      img: '/img/master-card.png',
+      number: '*** *** **** 4326',
+      purchaseDate: 'Purchase made on 07/20/2020 at 20:32'
+    }
+  },
+  {
+    img: 'https://api.lorem.space/image/game?w=151&h=71',
+    title: 'Red Dead Redemption 2',
+    price: 'R$ 215,00',
+    downloadLink: 'https://api.lorem.space/image/game?w=151&h=73',
+    paymentInfo: {
+      flag: 'visa',
+      img: '/img/visa.png',
+      number: '*** *** **** 5423',
+      purchaseDate: 'Purchase made on 08/20/2020 at 21:12'
+    }
+  }
+]
