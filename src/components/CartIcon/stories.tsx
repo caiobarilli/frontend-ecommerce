f490026534f3e4d74538cc26e0526febbb4313d7
Caio Barilli <caiobarilli@gmail.com>
2022-07-27 08:42:24 -0300

    Add CartIcon Component

---
+++ src/components/CartIcon/stories.tsx
@@ -0,0 +1,20 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import CartIcon, { CartIconProps } from '.'
+
+export default {
+  title: 'SINGLE COMPONENTS/CartIcon',
+  component: CartIcon,
+  parameters: {
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+} as Meta
+
+export const Default: Story = () => <CartIcon />
+
+export const withItems: Story<CartIconProps> = (args) => <CartIcon {...args} />
+
+withItems.args = {
+  quantity: 3
+}
