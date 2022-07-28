bcb58ac4c2eb14ae8518913474cdb05855799c54
Caio Barilli <caiobarilli@gmail.com>
2022-07-28 18:55:16 -0300

    Add test on UserDropdown component

--- src/components/UserDropdown/test.tsx
+++ src/components/UserDropdown/test.tsx
@@ -1,25 +1,32 @@
-// import { screen } from '@testing-library/react'
+import { screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { renderWithTheme } from 'utils/tests/helpers'
-// import userEvent from '@testing-library/user-event'
 
 import UserDropdown from '.'
 
 describe('<UserDropdown />', () => {
   it('should render the username', () => {
-    renderWithTheme(<UserDropdown username="Nextjs" />)
+    renderWithTheme(<UserDropdown username="next" />)
+    expect(screen.getByText(/next/i)).toBeInTheDocument()
   })
 
-  it('should render the menu', () => {
-    renderWithTheme(<UserDropdown username="User" />)
+  it('should render the menu', async () => {
+    renderWithTheme(<UserDropdown username="next" />)
 
     // open menu
-    // userEvent.click(screen.getByText(/User/i))
+    userEvent.click(screen.getByText(/next/i))
 
-    // expect(
-    //   screen.getByRole('link', { name: /my profile/i })
-    // ).toBeInTheDocument()
+    await waitFor(() => {
+      expect(
+        screen.getByRole('link', { name: /my profile/i })
+      ).toBeInTheDocument()
 
-    // expect(screen.getByRole('link', { name: /wishlist/i })).toBeInTheDocument()
-    // expect(screen.getByRole('link', { name: /sign out/i })).toBeInTheDocument()
+      expect(
+        screen.getByRole('link', { name: /wishlist/i })
+      ).toBeInTheDocument()
+      expect(
+        screen.getByRole('link', { name: /sign out/i })
+      ).toBeInTheDocument()
+    })
   })
 })
