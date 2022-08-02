5b2dab387de1433ef06b6eebfe8badae4304f809
Caio Barilli <caiobarilli@gmail.com>
2022-08-02 04:51:14 -0300

    Add Apollo, Graphql, Games page

--- jest.config.js
+++ jest.config.js
@@ -4,11 +4,16 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: [
     'src/**/*.ts(x)?',
-    '!src/types/**/*.d.ts',
-    '!src/pages/**/*.tsx',
-    '!src/styles/**/*.ts',
-    '!src/**/stories.tsx',
-    '!src/**/mock.ts'
+    'src/components/**/*.{js,jsx,ts,tsx}',
+    '!**/src/types/**/*.d.ts/**',
+    '!**/src/pages/**/*.tsx/**',
+    '!**/src/pages/*.tsx/**',
+    '!**/src/styles/**/*.ts/**',
+    '!**/src/graphql/queries/games.ts **',
+    '!**/src/utils/*.ts/**',
+    '!**/src/utils/**/*.ts/**',
+    '!**/src/**/mock.ts/**',
+    '!**/src/**/stories.tsx/**'
   ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,15 +1,39 @@
 import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
 import filterItemsMock from 'components/ExploreSidebar/mock'
-import gamesMock from 'components/CardSlider/mock'
+import { initializeApollo } from 'utils/apollo'
+
+import { QUERY_GAMES } from 'graphql/queries/games'
 
 export default function GamesPage(props: GamesTemplateProps) {
   return <GamesTemplate {...props} />
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query({ query: QUERY_GAMES })
+
   return {
     props: {
-      games: gamesMock,
+      revalidate: 60,
+      games: data.games.map(
+        (game: {
+          name: any
+          cover: { url: any }
+          developers: { name: any }[]
+          price: number | bigint
+        }) => ({
+          title: game.name,
+          image: game.cover
+            ? game.cover.url
+            : 'https://via.placeholder.com/150',
+          developer: game.developers[0].name,
+          price: new Intl.NumberFormat('en', {
+            style: 'currency',
+            currency: 'USD'
+          }).format(game.price)
+        })
+      ),
       filterItems: filterItemsMock
     }
   }
