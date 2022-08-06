7a073606c064bbf8fb7982bd1adfa85a33835cf0
Caio Barilli <caiobarilli@gmail.com>
2022-08-06 04:14:47 -0300

    add slug to card mock

--- src/components/CardSlider/mock.ts
+++ src/components/CardSlider/mock.ts
@@ -4,41 +4,47 @@ export default [
     developer: 'Rockstar Games',
     image: 'https://api.lorem.space/image/game?w=300&h=140',
     price: 'R$ 235,00',
-    promotionalPrice: 'R$ 215,00'
+    promotionalPrice: 'R$ 215,00',
+    slug: 'population-zero'
   },
   {
     title: 'Population Zero',
     developer: 'Rockstar Games',
     image: 'https://api.lorem.space/image/game?w=300&h=141',
     price: 'R$ 235,00',
-    promotionalPrice: 'R$ 215,00'
+    promotionalPrice: 'R$ 215,00',
+    slug: 'population-zero'
   },
   {
     title: 'Population Zero',
     developer: 'Rockstar Games',
     image: 'https://api.lorem.space/image/game?w=300&h=142',
     price: 'R$ 235,00',
-    promotionalPrice: 'R$ 215,00'
+    promotionalPrice: 'R$ 215,00',
+    slug: 'population-zero'
   },
   {
     title: 'Population Zero',
     developer: 'Rockstar Games',
     image: 'https://api.lorem.space/image/game?w=300&h=143',
     price: 'R$ 235,00',
-    promotionalPrice: 'R$ 215,00'
+    promotionalPrice: 'R$ 215,00',
+    slug: 'population-zero'
   },
   {
     title: 'Population Zero',
     developer: 'Rockstar Games',
     image: 'https://api.lorem.space/image/game?w=300&h=144',
     price: 'R$ 235,00',
-    promotionalPrice: 'R$ 215,00'
+    promotionalPrice: 'R$ 215,00',
+    slug: 'population-zero'
   },
   {
     title: 'Population Zero',
     developer: 'Rockstar Games',
     image: 'https://api.lorem.space/image/game?w=300&h=145',
     price: 'R$ 235,00',
-    promotionalPrice: 'R$ 215,00'
+    promotionalPrice: 'R$ 215,00',
+    slug: 'population-zero'
   }
 ]
