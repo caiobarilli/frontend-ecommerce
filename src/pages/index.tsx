8334ab9f0fdfa9c2e0e5ea1300b72effe92d650a
Caio Barilli <caiobarilli@gmail.com>
2022-08-08 07:11:51 -0300

    Add mapper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,8 @@ import { initializeApollo } from 'utils/apollo'
 import { QUERY_HOME } from 'graphql/queries/home'
 import { QueryHome, QueryHomeVariables } from 'graphql/generated/QueryHome'
 
+import { bannerMapper, gamesMapper, highlightMapper } from 'utils/mapper'
+
 export default function Index(props: HomeProps) {
   return <Home {...props} />
 }
@@ -24,95 +26,15 @@ export async function getStaticProps() {
   return {
     props: {
       revalidate: 60, // 1 minute (in seconds)
-      banners: banners.map((banner) => ({
-        imageUrl: banner.image?.url,
-        title: banner.title,
-        subtitle: banner.subtitle,
-        buttonText: banner.button?.label,
-        buttonLink: banner.button?.link,
-        ...(banner.ribbon && {
-          ribbon: banner.ribbon,
-          ribbonColor: banner.ribbon?.color,
-          ribbonSize: banner.ribbon?.size
-        })
-      })),
-      newGames: newGames.map((game) => ({
-        title: game.name,
-        developer: game.developers[0].name,
-        image: game.cover?.url,
-        price: new Intl.NumberFormat('en', {
-          style: 'currency',
-          currency: 'USD'
-        }).format(game.price),
-        slug: game.slug
-      })),
-      mostPopularHighlight: {
-        title: sections?.popularGames?.highlight?.title,
-        subtitle: sections?.popularGames?.highlight?.subtitle,
-        buttonLabel: sections?.popularGames?.highlight?.buttonLabel,
-        buttonUrl: sections?.popularGames?.highlight?.buttonLink,
-        backgroundUrl: sections?.popularGames?.highlight?.background?.url,
-        imageUrl: sections?.popularGames?.highlight?.floatImage?.url,
-        alignment: sections?.popularGames?.highlight?.alignment
-      },
-      mostPopularGames: sections?.popularGames?.games?.map((game) => ({
-        title: game.name,
-        developer: game.developers[0].name,
-        image: game.cover?.url,
-        price: new Intl.NumberFormat('en', {
-          style: 'currency',
-          currency: 'USD'
-        }).format(game.price),
-        slug: game.slug
-      })),
-      upcommingGames: upcomingGames.map((game) => ({
-        title: game.name,
-        developer: game.developers[0].name,
-        image: game.cover?.url,
-        price: new Intl.NumberFormat('en', {
-          style: 'currency',
-          currency: 'USD'
-        }).format(game.price),
-        slug: game.slug
-      })),
-      upcomminghighlight: {
-        title: sections?.upcomingGames?.highlight?.title,
-        subtitle: sections?.upcomingGames?.highlight?.subtitle,
-        buttonLabel: sections?.upcomingGames?.highlight?.buttonLabel,
-        buttonUrl: sections?.upcomingGames?.highlight?.buttonLink,
-        backgroundUrl: sections?.upcomingGames?.highlight?.background?.url,
-        imageUrl: sections?.upcomingGames?.highlight?.floatImage?.url,
-        alignment: sections?.upcomingGames?.highlight?.alignment
-      },
-      upcommingMoreGames: upcomingGames.map((game) => ({
-        title: game.name,
-        developer: game.developers[0].name,
-        image: game.cover?.url,
-        price: new Intl.NumberFormat('en', {
-          style: 'currency',
-          currency: 'USD'
-        }).format(game.price),
-        slug: game.slug
-      })),
-      freeGames: freeGames.map((game) => ({
-        title: game.name,
-        developer: game.developers[0].name,
-        image: game.cover?.url,
-        price: new Intl.NumberFormat('en', {
-          style: 'currency',
-          currency: 'USD'
-        }).format(game.price),
-        slug: game.slug
-      })),
-      freehighlight: {
-        title: sections?.freeGames?.highlight?.title,
-        subtitle: sections?.freeGames?.highlight?.subtitle,
-        buttonLabel: sections?.freeGames?.highlight?.buttonLabel,
-        buttonUrl: sections?.freeGames?.highlight?.buttonLink,
-        backgroundUrl: sections?.freeGames?.highlight?.background?.url,
-        imageUrl: sections?.freeGames?.highlight?.floatImage?.url,
-        alignment: sections?.freeGames?.highlight?.alignment
-      }
+      banners: bannerMapper(banners),
+      newGames: gamesMapper(newGames),
+      mostPopularHighlight: highlightMapper(sections?.popularGames?.highlight),
+      mostPopularGames: gamesMapper(sections?.popularGames?.games),
+      upcommingGames: gamesMapper(upcomingGames),
+      upcomminghighlight: highlightMapper(sections?.upcomingGames?.highlight),
+      upcommingMoreGames: gamesMapper(upcomingGames),
+      freeGames: gamesMapper(freeGames),
+      freehighlight: highlightMapper(sections?.freeGames?.highlight)
     }
   }
 }
---
+++ src/utils/mapper.ts
@@ -0,0 +1,49 @@
+import {
+  QueryHome_banners,
+  QueryHome_newGames,
+  QueryHome_sections_freeGames_highlight
+} from 'graphql/generated/QueryHome'
+
+export const bannerMapper = (banners: QueryHome_banners[]) => {
+  return banners.map((banner) => ({
+    imageUrl: banner.image?.url,
+    title: banner.title,
+    subtitle: banner.subtitle,
+    buttonText: banner.button?.label,
+    buttonLink: banner.button?.link,
+    ...(banner.ribbon && {
+      ribbon: banner.ribbon,
+      ribbonColor: banner.ribbon?.color,
+      ribbonSize: banner.ribbon?.size
+    })
+  }))
+}
+
+export const gamesMapper = (games: QueryHome_newGames[] | null | undefined) => {
+  return games?.map((game) => ({
+    title: game.name,
+    developer: game.developers[0].name,
+    image: game.cover?.url,
+    price: new Intl.NumberFormat('en', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(game.price),
+    slug: game.slug
+  }))
+}
+
+export const highlightMapper = (
+  highlight: QueryHome_sections_freeGames_highlight | null | undefined
+) => {
+  return highlight
+    ? {
+        title: highlight.title,
+        subtitle: highlight?.subtitle,
+        buttonLabel: highlight?.buttonLabel,
+        buttonUrl: highlight?.buttonLink,
+        backgroundUrl: highlight?.background?.url,
+        imageUrl: highlight?.floatImage?.url,
+        alignment: highlight?.alignment
+      }
+    : {}
+}
