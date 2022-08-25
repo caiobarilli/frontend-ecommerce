b77765fdf736676e10b322da10e1c5524f6893df
Caio Barilli <caiobarilli@gmail.com>
2022-08-25 11:40:50 -0300

    Editando CartList para usar o useCart hook

--- src/components/CartList/mock.ts
+++ src/components/CartList/mock.ts
@@ -1,10 +1,12 @@
 export default [
   {
+    id: '1',
     img: 'https://api.lorem.space/image/game?w=1042&h=581',
     title: 'Red Dead Redemption 2',
     price: 'R$ 215,00'
   },
   {
+    id: '2',
     img: 'https://api.lorem.space/image/game?w=1042&h=580',
     title: 'Borderlands 3',
     price: 'R$ 215,00'
--- src/components/CartList/test.tsx
+++ src/components/CartList/test.tsx
@@ -1,27 +1,38 @@
-import { screen } from '@testing-library/react'
-import { renderWithTheme } from 'utils/tests/helpers'
+import { CartContextDefaultValues } from 'hooks/use-cart'
+import { render, screen } from 'utils/test-utils'
 
 import CartList from '.'
 import mockItems from './mock'
 
 describe('<CartList />', () => {
+  it('should render empty if there are no games', () => {
+    render(<CartList />)
+
+    expect(screen.getByText(/your cart is empty/i)).toBeInTheDocument()
+    expect(screen.queryByText(/total/i)).not.toBeInTheDocument()
+  })
+
   it('should render the cart list', () => {
-    renderWithTheme(<CartList items={mockItems} total="R$ 330,00" />)
+    const cartProviderProps = {
+      ...CartContextDefaultValues,
+      items: mockItems,
+      total: 'R$ 330,00'
+    }
+
+    render(<CartList />, { cartProviderProps })
 
     expect(screen.getAllByRole('heading')).toHaveLength(2)
     expect(screen.getByText('R$ 330,00')).toHaveStyle({ color: '#F231A5' })
   })
 
   it('should render the button', () => {
-    renderWithTheme(<CartList items={mockItems} total="R$ 330,00" hasButton />)
+    const cartProviderProps = {
+      ...CartContextDefaultValues,
+      items: mockItems
+    }
+
+    render(<CartList hasButton />, { cartProviderProps })
 
     expect(screen.getByText(/buy it now/i)).toBeInTheDocument()
-  })
-
-  it('should render empty if there are no games', () => {
-    renderWithTheme(<CartList />)
-
-    expect(screen.getByText(/your cart is empty/i)).toBeInTheDocument()
-    expect(screen.queryByText(/total/i)).not.toBeInTheDocument()
   })
 })
